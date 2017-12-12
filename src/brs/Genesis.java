d8ec2b5607d238bf99cbc9df9c632a978f27be3a
Cryptoinn <inneluki@gmail.com>
2017-12-12 18:30:59 +0300

    Update Genesis.java
    new signature

--- src/brs/Genesis.java
+++ src/brs/Genesis.java
@@ -2,11 +2,11 @@ package brs;
 
 public final class Genesis {
 
-  public static final long GENESIS_BLOCK_ID = 3444294670862540038L;
+  public static final long GENESIS_BLOCK_ID = 1109294670862540038L;
   public static final long CREATOR_ID = 0L;
   public static final byte[] CREATOR_PUBLIC_KEY = {
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+    1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0,
+    0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1,
   };
 
   public static final long[] GENESIS_RECIPIENTS = {};
@@ -16,8 +16,8 @@ public final class Genesis {
   public static final byte[][] GENESIS_SIGNATURES = {};
 
   public static final byte[] GENESIS_BLOCK_SIGNATURE = new byte[]{
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    1, 2, 5, 7, 3, 1, 6, 2, 0, 6, 1, 0, 2, 0, 0, 9,
+    1, 1, 0, 9, 9, 8, 5, 3, 2, 7, 9, 1, 0, 0, 2, 0,
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
   };
